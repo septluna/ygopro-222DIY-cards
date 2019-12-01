68bb30df1d76e52dbfad87e8330dce2196351e18
TanakaKotoha <miho_kohinata@hotmail.com>
2019-12-01 16:14:32 +0800

    fix

--- expansions/script/c17050932.lua
+++ expansions/script/c17050932.lua
@@ -38,6 +38,30 @@ function cm.initial_effect(c)
 	e3:SetTarget(cm.distg)
 	e3:SetOperation(cm.disop)
 	c:RegisterEffect(e3)
+	--spsummon voice
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e7:SetCode(EVENT_SUMMON_SUCCESS)
+	e7:SetOperation(cm.sumsuc)
+	c:RegisterEffect(e7)
+	local e8=e7:Clone()
+	e8:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e8)
+	--atk voice
+	local e9=Effect.CreateEffect(c)
+	e9:SetCategory(CATEGORY_ATKCHANGE)
+	e9:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e9:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e9:SetOperation(cm.atksuc)
+	c:RegisterEffect(e9)
+	--destroy voice
+	local e10=Effect.CreateEffect(c)
+	e10:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e10:SetCode(EVENT_DESTROYED)
+	e10:SetProperty(EFFECT_FLAG_DELAY)
+	e10:SetCondition(cm.descon)
+	e10:SetOperation(cm.dessuc)
+	c:RegisterEffect(e10)
 end
 function cm.lcheck(g)
 	return g:GetClassCount(Card.GetLinkAttribute)==g:GetCount()
@@ -83,6 +107,7 @@ function cm.discon(e,tp,eg,ep,ev,re,r,rp)
 	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
 function cm.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SOUND,0,aux.Stringid(17030012,4))
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
@@ -93,4 +118,17 @@ function cm.disop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
+end
+function cm.descon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE+LOCATION_SZONE) and c:IsFaceup()
+end
+function cm.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SOUND,0,aux.Stringid(17030012,2))
+end	
+function cm.atksuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SOUND,0,aux.Stringid(17030012,3))
+end
+function cm.dessuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SOUND,0,aux.Stringid(17030012,5))
 end
