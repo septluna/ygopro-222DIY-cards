29157916f060f28d9022822783c99ba8d6b4b963
TanakaKotoha <miho_kohinata@hotmail.com>
2019-12-30 15:19:50 +0800

    fix touha

--- expansions/script/c81022011.lua
+++ expansions/script/c81022011.lua
@@ -1,6 +1,6 @@
 --黑翼之声·天空桥朋花
-c81022011.card_code_list={81022000}
 function c81022011.initial_effect(c)
+	aux.AddCodeList(c,81022000)
 	c:EnableReviveLimit()
 	--code
 	local e1=Effect.CreateEffect(c)
