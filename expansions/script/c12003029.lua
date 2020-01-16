c6d5fe8f794d557607c51810171f38545d6030da
gg123gg <32418512+gg123gg@users.noreply.github.com>
2020-01-16 15:35:57 +0800

    fix

--- expansions/script/c12003029.lua
+++ expansions/script/c12003029.lua
@@ -2,105 +2,98 @@
 local m=12003029
 local cm=_G["c"..m]
 function cm.initial_effect(c)
-	--special summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(m,2))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,m)
-	e1:SetTarget(cm.sctg)
-	e1:SetOperation(cm.scop)
-	c:RegisterEffect(e1)
-	--spsummon
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(m,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,m+100)
-	e2:SetCondition(cm.spcon)
-	e2:SetTarget(cm.sptg)
-	e2:SetOperation(cm.spop)
-	c:RegisterEffect(e2)
+    --special summon
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(m,2))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,m)
+    e1:SetTarget(cm.sctg)
+    e1:SetOperation(cm.scop)
+    c:RegisterEffect(e1)
+    --spsummon
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(m,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetCountLimit(1,m+100)
+    e2:SetCondition(cm.spcon)
+    e2:SetTarget(cm.sptg)
+    e2:SetOperation(cm.spop)
+    c:RegisterEffect(e2)
 end
 function cm.scfilter1(c,e,tp,mc)
-	local mg=Group.FromCards(c,mc)
-	return not c:IsType(TYPE_TUNER) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingMatchingCard(cm.scfilter2,tp,LOCATION_EXTRA,0,1,nil,mg)
+    local mg=Group.FromCards(c,mc)
+    return not c:IsType(TYPE_TUNER) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+        and Duel.IsExistingMatchingCard(cm.scfilter2,tp,LOCATION_EXTRA,0,1,nil,mg)
 end
 function cm.scfilter2(c,mg)
-	return c:IsSynchroSummonable(nil,mg)
+    return c:IsSynchroSummonable(nil,mg)
 end
 function cm.sctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_HAND) and chkc:IsControler(tp) and cm.scfilter1(chkc,e,tp,e:GetHandler()) end
-	if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,2)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(cm.scfilter1,tp,LOCATION_HAND,0,1,nil,e,tp,e:GetHandler()) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+    if chkc then return chkc:IsLocation(LOCATION_HAND) and chkc:IsControler(tp) and cm.scfilter1(chkc,e,tp,e:GetHandler()) end
+    local c=e:GetHandler()
+    if chk==0 then return Duel.IsPlayerCanSpecialSummonCount(tp,3)
+        and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(cm.scfilter1,tp,LOCATION_HAND,0,1,nil,e,tp,e:GetHandler()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function cm.scop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
-	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tc=Duel.SelectMatchingCard(tp,cm.scfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp,e:GetHandler())
-	if not tc:IsRelateToEffect(e) or not c:IsRelateToEffect(e) or not Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) or not Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) then return end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	tc:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_DISABLE_EFFECT)
-	tc:RegisterEffect(e2)
-	Duel.SpecialSummonComplete()
-	if not c:IsRelateToEffect(e) then return end
-	local mg=Group.FromCards(c,tc)
-	local g=Duel.GetMatchingGroup(cm.scfilter2,tp,LOCATION_EXTRA,0,nil,mg)
-	if g:GetCount()>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,1,1,nil)
-		local e1=Effect.CreateEffect(c)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-		e1:SetValue(LOCATION_DECKBOT)
-		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
-		c:RegisterEffect(e1,true)
-		local e2=e1:Clone()
-		tc:RegisterEffect(e2,true)
-		Duel.SynchroSummon(tp,sg:GetFirst(),nil,mg)
-	end
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
+    local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local mc=Duel.SelectMatchingCard(tp,cm.scfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp,e:GetHandler())
+    local tc=mc:GetFirst()
+    Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+    Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_DISABLE)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+    tc:RegisterEffect(e1)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_DISABLE_EFFECT)
+    tc:RegisterEffect(e2)
+    Duel.SpecialSummonComplete()
+    local mg=Group.FromCards(c,tc)
+    local g=Duel.GetMatchingGroup(cm.scfilter2,tp,LOCATION_EXTRA,0,nil,mg)
+    if g:GetCount()>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sg=g:Select(tp,1,1,nil)
+        Duel.SynchroSummon(tp,sg:GetFirst(),nil,mg)
+    end
 end
 function cm.con(e,tp,eg,ep,ev,re,r,rp)
-	return not e:GetHandler():IsReason(REASON_DRAW)
+    return not e:GetHandler():IsReason(REASON_DRAW)
 end
 function cm.filter(c)
-	return c:IsSetCard(0xfb8) and not c:IsCode(m) and c:IsAbleToHand()
+    return c:IsSetCard(0xfb8) and not c:IsCode(m) and c:IsAbleToHand()
 end
 function cm.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_DECK) and cm.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(cm.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_SEARCH,nil,1,tp,LOCATION_DECK)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_DECK) and cm.filter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(cm.filter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_SEARCH,nil,1,tp,LOCATION_DECK)
 end
 function cm.op(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local tc=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_DECK,0,1,1,nil)
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local tc=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_DECK,0,1,1,nil)
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,tc)
 end
 function cm.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsReason(REASON_COST) and re:IsHasType(0x7e0) and re:IsActiveType(TYPE_MONSTER)
-		and re:GetHandler():IsAttribute(ATTRIBUTE_WATER)
+    return e:GetHandler():IsReason(REASON_COST) and re:IsHasType(0x7e0) and re:IsActiveType(TYPE_MONSTER)
+        and re:GetHandler():IsAttribute(ATTRIBUTE_WATER)
 end
 function cm.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
 end
 function cm.spop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
-	Duel.HintSelection(dg)
-	Duel.SendtoGrave(dg,REASON_EFFECT)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+    local dg=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,0,LOCATION_ONFIELD,1,1,nil)
+    Duel.HintSelection(dg)
+    Duel.SendtoGrave(dg,REASON_EFFECT)
 end
