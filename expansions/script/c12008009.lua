788ab18fbf7aa83d40ad26683fc344c04e2dbd3f
gg123gg <32418512+gg123gg@users.noreply.github.com>
2020-02-11 10:13:42 +0800

    fix

--- expansions/script/c12008009.lua
+++ expansions/script/c12008009.lua
@@ -5,13 +5,13 @@ function c12008009.initial_effect(c)
 	c:EnableReviveLimit()
 	--Destroy
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(12008009,2))
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_EXTRA)
 	e1:SetCondition(c12008009.linkcon)
 	e1:SetOperation(c12008009.linkop)
-	e1:SetValue(SUMMON_TYPE_LINK)
 	c:RegisterEffect(e1)
 	
 	local e1=Effect.CreateEffect(c)
@@ -39,10 +39,10 @@ function c12008009.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function c12008009.sprfilter(c,sc)
-	return c:IsCanBeLinkMaterial(sc) and c:IsRace(RACE_SEASERPENT) and Duel.IsExistingMatchingCard(c12008009.sprfilter1,tp,LOCATION_MZONE+LOCATION_HAND,0,1,c) and Duel.GetLocationCountFromEx(tp,tp,c,TYPE_LINK)>0
+	return c:IsCanBeLinkMaterial(sc) and c:IsRace(RACE_SEASERPENT) and Duel.IsExistingMatchingCard(c12008009.sprfilter1,tp,LOCATION_MZONE+LOCATION_HAND,0,1,c) and Duel.GetLocationCountFromEx(tp,tp,c,TYPE_LINK)>0 and c:IsAbleToRemoveAsCost()
 end
 function c12008009.sprfilter1(c,sc)
-	return c:IsCanBeLinkMaterial(sc) and c:IsRace(RACE_MACHINE) and Duel.GetLocationCountFromEx(tp,tp,c,TYPE_LINK)>0
+	return c:IsCanBeLinkMaterial(sc) and c:IsRace(RACE_MACHINE) and Duel.GetLocationCountFromEx(tp,tp,c,TYPE_LINK)>0 and c:IsAbleToRemoveAsCost()
 end
 function c12008009.linkcon(e,c)
 	if c==nil then return true end
@@ -54,11 +54,11 @@ function c12008009.linkop(e,tp,eg,ep,ev,re,r,rp,c)
 	local c=e:GetHandler()
 	local g=Duel.GetMatchingGroup(c12008009.sprfilter,tp,LOCATION_MZONE+LOCATION_HAND,0,nil,c)
 	if g:GetCount()>0 then
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local cc1=g:Select(tp,1,1,nil)
 	local cc2=Duel.SelectMatchingCard(tp,c12008009.sprfilter1,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,cc1)
 	cc1:Merge(cc2)
-	Duel.Release(cc1,REASON_EFFECT)
+	Duel.Remove(cc1,POS_FACEUP,REASON_EFFECT)
 	end
 end
 function c12008009.filter(c)
