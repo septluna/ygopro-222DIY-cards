fb5758e4af97908cd7a2203c0d1bded21fbc2cc7
gg123gg <32418512+gg123gg@users.noreply.github.com>
2020-02-17 22:53:15 +0800

    Add files via upload

--- expansions/script/c12001018.lua
+++ expansions/script/c12001018.lua
@@ -18,104 +18,7 @@ function c12001018.initial_effect(c)
 --  
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(12001018,2))
-	e2:SetCategory(CATEGORY_TOGRAVE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetCountLimit(1,12001104)
-	e2:SetCondition(c12001018.con2)
-	e2:SetOperation(c12001018.op2)
-	c:RegisterEffect(e2)
---
-end
---
-function c12001018.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
-end
---
-function c12001018.op1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1_1=Effect.CreateEffect(c)
-	e1_1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1_1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1_1:SetLabelObject(c)
-	e1_1:SetCondition(c12001018.con1_1)
-	e1_1:SetOperation(c12001018.op1_1)
-	e1_1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1_1,tp)
---
-	local e1_2=Effect.CreateEffect(c)
-	e1_2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e1_2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1_2:SetCondition(c12001018.con1_2)
-	e1_2:SetOperation(c12001018.op1_2)
-	e1_2:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1_2,tp)
-	local e1_3=Effect.CreateEffect(c)
-	e1_3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e1_3:SetCode(EVENT_CHAIN_SOLVED)
-	e1_3:SetLabelObject(c)
-	e1_3:SetCondition(c12001018.con1_3)
-	e1_3:SetOperation(c12001018.op1_3)
-	e1_3:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1_3,tp)
---
-end
---
-function c12001018.cfilter1_1(c,tp)
-	return c:GetSummonPlayer()==1-tp
-end
-function c12001018.con1_1(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c12001018.cfilter1_1,1,nil,tp) 
-		and (not re:IsHasType(EFFECT_TYPE_ACTIONS) or re:IsHasType(EFFECT_TYPE_CONTINUOUS))
-end
-function c12001018.op1_1(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsPlayerCanDiscardDeck(tp,1) then return end
-	if not Duel.SelectYesNo(tp,aux.Stringid(12001018,1)) then return end
-	Duel.ConfirmDecktop(tp,1)
-	local g=Duel.GetDecktopGroup(tp,1)
-	local tc=g:GetFirst()
-	local c=e:GetLabelObject()
-	if tc:IsSetCard(0xfb0) then
-		Duel.DisableShuffleCheck()
-		Duel.SendtoGrave(tc,REASON_EFFECT+REASON_REVEAL)
-	else
-		if c:IsLocation(LOCATION_GRAVE) and tc:IsAbleToHand() then
-			Duel.DisableShuffleCheck()
-			Duel.SendtoDeck(tc,nil,1,REASON_EFFECT)
-			Duel.ShuffleHand(tp)
-		else
-			Duel.ShuffleDeck(tp)
-		end
-	end
-end
---
-function c12001018.con1_2(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c12001018.cfilter1_1,1,nil,tp) 
-		and re:IsHasType(EFFECT_TYPE_ACTIONS) and not re:IsHasType(EFFECT_TYPE_CONTINUOUS)
-end
---六曜 赤口的完红丘依儿
-function c12001018.initial_effect(c)
-	--pendulum summon
-	aux.EnablePendulumAttribute(c)
-	--
-
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(12001018,0))
-	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_DECKDES)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0,0x1c0)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,12001018)
-	e1:SetCost(c12001018.cost1)
-	e1:SetOperation(c12001018.op1)
-	c:RegisterEffect(e1)
---  
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(12001018,2))
-	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetCategory(CATEGORY_REMOVE)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
@@ -227,7 +130,6 @@ function c12001018.tfilter2(c)
 end
 function c12001018.dstg(e,tp,eg,ep,ev,re,r,rp)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_MZONE)
 end
 function c12001018.dsop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,1-tp,HINTMSG_REMOVE)
