18d5270186c0f83f2bdb3d06aeb6910696593141
gg123gg <32418512+gg123gg@users.noreply.github.com>
2020-02-23 22:11:13 +0800

    Update c12005023.lua

--- expansions/script/c12005023.lua
+++ expansions/script/c12005023.lua
@@ -66,7 +66,7 @@ function cm.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function cm.spfilter(c,e,tp)
-	return c:IsSetCard(0xfbb) or c:IsSetCard(0x1fbd) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return ( c:IsSetCard(0xfbb) or c:IsSetCard(0x1fbd) ) and c:IsType(TYPE_MONSTER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function cm.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and cm.spfilter(chkc,e,tp) end
