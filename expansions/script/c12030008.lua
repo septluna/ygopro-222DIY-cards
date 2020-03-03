41eb88cc23be1ded0faa4a07ef87017c4314c8ad
gg123gg <32418512+gg123gg@users.noreply.github.com>
2020-03-03 22:04:46 +0800

    Update c12030008.lua

--- expansions/script/c12030008.lua
+++ expansions/script/c12030008.lua
@@ -81,7 +81,7 @@ function c12030008.disop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(dm)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE+RESET_PHASE+PHASE_STANDBY,2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
 		c:RegisterEffect(e1)
    end
 end
