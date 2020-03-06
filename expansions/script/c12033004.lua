203c6f03c4f56b09f50aa4709fd50a90816f86f8
gg123gg <32418512+gg123gg@users.noreply.github.com>
2020-03-06 11:40:38 +0800

    Update c12033004.lua

--- expansions/script/c12033004.lua
+++ expansions/script/c12033004.lua
@@ -56,10 +56,10 @@ function cm.operation(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=g:GetFirst()
 	while tc do
 	local code=tc:GetOriginalCode()
-		   if c:IsRelateToEffect(e) and c:IsFaceup() then
+		   if c:IsFaceup() then
 			  if c:GetFlagEffect(code)>0 then return end
-				c:CopyEffect(code,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,1)
-				c:RegisterFlagEffect(code,0x1fe0000,0,1)
+				c:CopyEffect(code,RESET_EVENT+0x1fe0000,1)
+				c:RegisterFlagEffect(code,RESET_EVENT+0x1fe0000,0,1)
 			end
 	tc=g:GetNext() 
 	end
